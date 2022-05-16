641c25abb7ce3a05de6b908c7d23358577519a56
Checksum <checksum@velvet.moe>
2022-05-15 19:29:17 -0500

    feat: :sparkles: Calcuate HWID based on system hardware without the user having to specify it directly.
    This is a complete refractor, PR's welcome!
    
    BREAKING CHANGES: Users can no longer specify keys and values manually, this is grabbed from `sysinfo`. Please revise your code.

--- README.md
+++ README.md
@@ -4,23 +4,19 @@
 
 ## Example usage
 
-```rust
-use uniqueid;
-
-pub fn main() {
-    let data = vec![
-        identifier_data!("Vendor", "Intel"),
-        identifier_data!("Model", "Xeon E5-2670"),
-    ];
-
-    let type_ = identifier_type!(CPU, data);
-
-    let identifier = uniqueid::identifier::Identifier::new(Some("HWID"), vec![type_.clone()]);
-
-    let output = identifier.build(false); // true = hash
-
-    println!("{}", output); // Outputs HWID[CPU(Vendor=Intel, Model=Xeon E5-2670)]
-}
+ ```rust
+ use uniqueid::IdentifierBuilder;
+ 
+ let identifier = IdentifierBuilder::new()
+     .name("HWID") // optional
+     .add(IdentifierTypeName::CPU)
+     .add(IdentifierTypeName::RAM) 
+     .add(IdentifierTypeName::DISK)
+     .build();
+ 
+ let result = identifier.build(false); // false = don't hash the output
+ 
+ println!("{}", result);
 ```
 
 ___
@@ -39,7 +35,7 @@ A basic identifier would look like this:
 
 NAME[TYPE(a=b, ...), ...]
 
-NAME - Optional, defaults to None
+NAME - Optional name of the identifier. (Can be used as a label or salt)
 TYPE - The type of identifier, e.g. CPU, GPU, etc.
 DATA - The data for the identifier, e.g. Vendor=Intel, Model=Xeon E5-2670
 
@@ -48,7 +44,9 @@ This is a very basic example of an identifier, and most use cases will have more
 
 ## Roadmap
 
-- [ ] Calculate the HWID **based on the system's hardware** in the library without needing to specify it manually.
+- [X] Calculate the HWID **based on the system's hardware** in the library without needing to specify it manually.
+- [ ] Add support for other identifier types. (currently only supports CPU, RAM, and DISK) - **PR's welcome**!
+- [ ] Add disk serial checking.
 - [ ] Add support for other hashing algorithms. (currently only supports SHA3-512)
 
 ## License
--- src/identifier.rs
+++ src/identifier.rs
@@ -1,11 +1,111 @@
-use sha3::{Digest, Sha3_512, Sha3_512Core};
+use sha3::{Digest, Sha3_512};
+use sysinfo::{self, DiskExt, ProcessorExt, System, SystemExt};
 
+/// IdentifierTypeName are the available types of identifiers.
+/// # Examples
+/// ```
+/// use uniqueid::identifier::IdentifierTypeName;
+/// assert_eq!(IdentifierTypeName::CPU.as_str(), "CPU");
+/// ```
+/// # See Also
+/// * [IdentifierType](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierType.html)
+/// * [Identifier](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.Identifier.html)
+/// * [IdentifierTypeData](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierTypeData.html)
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum IdentifierTypeName {
+    CPU,
+    // GPU, // TODO: Add GPU support
+    RAM,
+    DISK,
+    // NET, // TODO: Add network identifier
+    // OS, // TODO: Add OS identifier
+}
+
+impl IdentifierTypeName {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            IdentifierTypeName::CPU => "CPU",
+            // IdentifierTypeName::GPU => "GPU",
+            IdentifierTypeName::RAM => "RAM",
+            IdentifierTypeName::DISK => "DISK",
+            // IdentifierTypeName::NET => "NET",
+            // IdentifierTypeName::OS => "OS",
+        }
+    }
+}
+
+impl From<&str> for IdentifierTypeName {
+    fn from(name: &str) -> Self {
+        match name {
+            "CPU" => IdentifierTypeName::CPU,
+            // "GPU" => IdentifierTypeName::GPU,
+            "RAM" => IdentifierTypeName::RAM,
+            "DISK" => IdentifierTypeName::DISK,
+            // "NET" => IdentifierTypeName::NET,
+            // "OS" => IdentifierTypeName::OS,
+            _ => panic!("Unknown identifier type name: {}", name),
+        }
+    }
+}
+
+/// The IdentifierTypeData object is the data associated with an IdentifierType.
+/// # Examples
+/// ```
+/// use uniqueid::identifier::IdentifierTypeData;
+///
+/// let data = IdentifierTypeData::new("key", "value");
+/// assert_eq!(data.key, "key");
+/// assert_eq!(data.value, "value");
+/// ```
+/// # See Also
+/// * [IdentifierType](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierType.html)
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct IdentifierTypeData {
     /// The key of the IdentifierTypeData object.
     pub key: String,
     /// The value of the IdentifierTypeData object.
     pub value: String,
+}
+
+pub struct IdentifierTypeDataBuilder {
+    r#type: IdentifierTypeName,
+    data: Vec<IdentifierTypeData>,
+}
+
+impl IdentifierTypeDataBuilder {
+    pub fn new(r#type: IdentifierTypeName) -> Self {
+        IdentifierTypeDataBuilder {
+            r#type,
+            data: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, key: &str, value: &str) -> &mut Self {
+        self.data.push(IdentifierTypeData {
+            key: key.to_string(),
+            value: value.to_string(),
+        });
+        self
+    }
+
+    pub fn build(self) -> String {
+        // Build the IdentifierTypeData objects in format:
+        // TYPE_NAME(key=value, key=value, key=value ...)
+        let mut data = String::new();
+        data.push_str(self.r#type.as_str());
+
+        data.push('(');
+
+        for item in self.data {
+            data.push_str(&format!("{}={}, ", item.key, item.value));
+        }
+        data.pop();
+        data.pop();
+
+        data.push(')');
+
+        data
+    }
 }
 
 impl IdentifierTypeData {
@@ -16,38 +116,128 @@ impl IdentifierTypeData {
             value: value.to_string(),
         }
     }
+
+    /// Returns the key of the IdentifierTypeData object.
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierTypeData;
+    /// let data = IdentifierTypeData::new("key", "value");
+    /// assert_eq!(data.key(), "key");
+    /// ```
+    pub fn key(&self) -> &str {
+        &self.key
+    }
+
+    /// Returns the value of the IdentifierTypeData object.
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierTypeData;
+    /// let data = IdentifierTypeData::new("key", "value");
+    /// assert_eq!(data.value(), "value");
+    /// ```
+    pub fn value(&self) -> &str {
+        &self.value
+    }
+
+    /// Returns the key and value in normal format. (key=value)
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierTypeData;
+    /// let data = IdentifierTypeData::new("key", "value");
+    /// assert_eq!(data.to_string(), "key=value");
+    /// ```
+    pub fn to_string(&self) -> String {
+        format!("{}={}", self.key, self.value)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-/// Contains the type of an identifier.
 pub struct IdentifierType {
     /// The name of the IdentifierType object.
-    pub name: String,
+    pub name: IdentifierTypeName,
     /// The data of the IdentifierType object.
     pub data: Vec<IdentifierTypeData>,
 }
 
 impl IdentifierType {
     /// Creates a new IdentifierType object.
-    pub fn new(name: String, data: Vec<IdentifierTypeData>) -> Self {
-        IdentifierType { name, data }
+    pub fn new(r#type: IdentifierTypeName) -> Self {
+        IdentifierType {
+            name: r#type,
+            data: Vec::new(),
+        }
     }
 
-    /// Builds the IdentifierType object.
     pub fn build(&self) -> String {
-        let mut result = String::new();
-        result.push_str(&self.name.to_string());
-        result.push('(');
-        for i in &self.data {
-            result.push_str(&i.key);
-            result.push('=');
-            result.push_str(&i.value);
-            result.push_str(", ");
+        match self.name {
+            IdentifierTypeName::CPU => self.build_cpu(),
+            // IdentifierTypeName::GPU => self.build_gpu(),
+            IdentifierTypeName::RAM => self.build_ram(),
+            IdentifierTypeName::DISK => self.build_disk(),
+            // IdentifierTypeName::NET => self.build_net(),
+            // IdentifierTypeName::OS => self.build_os(),
         }
+    }
 
-        result.pop();
-        result.pop();
-        result.push(')');
+    fn build_cpu(&self) -> String {
+        let mut sys = System::new_all();
+
+        sys.refresh_all();
+
+        let cpu = sys.processors();
+        let brand = cpu[0].brand();
+        let vendor = cpu[0].vendor_id();
+        let frequency = cpu[0].frequency();
+        let cores = cpu.len();
+
+        let mut result = String::new();
+
+        let mut identifier_type = IdentifierTypeDataBuilder::new(IdentifierTypeName::CPU);
+        identifier_type.add("b", brand.to_lowercase().trim());
+        identifier_type.add("v", vendor.to_lowercase().trim());
+        identifier_type.add("f", &frequency.to_string());
+        identifier_type.add("c", &cores.to_string());
+        result.push_str(&identifier_type.build());
+
+        result
+    }
+
+    fn build_ram(&self) -> String {
+        let mut sys = System::new_all();
+
+        sys.refresh_all();
+
+        let ram = sys.total_memory();
+
+        let mut result = String::new();
+
+        let mut identifier_type = IdentifierTypeDataBuilder::new(IdentifierTypeName::RAM);
+        identifier_type.add("t", &ram.to_string());
+        result.push_str(&identifier_type.build());
+
+        result
+    }
+
+    fn build_disk(&self) -> String {
+        let mut sys = System::new_all();
+
+        sys.refresh_all();
+
+        let disks = sys.disks();
+
+        let mut result = String::new();
+
+        for disk in disks {
+            if disk.is_removable() {
+                continue;
+            }
+
+            let total_space = disk.total_space();
+
+            let mut identifier_type = IdentifierTypeDataBuilder::new(IdentifierTypeName::DISK);
+            identifier_type.add("t", &total_space.to_string());
+            result.push_str(&identifier_type.build());
+        }
 
         result
     }
@@ -62,7 +252,6 @@ pub struct Identifier {
 }
 
 impl Identifier {
-    /// Creates a new Identifier object.
     pub fn new(name: Option<&str>, data: Vec<IdentifierType>) -> Identifier {
         Identifier {
             name: name.map(|s| s.to_string()),
@@ -98,5 +287,54 @@ impl Identifier {
         }
 
         result
+    }
+}
+
+/// Builds an Identifier object.
+/// # Examples
+/// ```
+/// use uniqueid::identifier::{IdentifierBuilder, IdentifierTypeName};
+///
+/// let identifier = IdentifierBuilder::new()
+///    .name("name") // Optional
+///     .add(IdentifierTypeName::CPU)
+///     .add(IdentifierTypeName::RAM)
+///     .add(IdentifierTypeName::DISK)
+///     .build();
+/// ```
+/// # See Also
+/// * [Identifier](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.Identifier.html)
+/// * [IdentifierType](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierType.html)
+/// * [IdentifierTypeName](https://docs.rs/uniqueid/latest/uniqueid/identifier/enum.IdentifierTypeName.html)
+/// * [IdentifierTypeData](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierTypeData.html)
+/// * [IdentifierTypeDataBuilder](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierTypeDataBuilder.html)
+pub struct IdentifierBuilder {
+    name: Option<String>,
+    data: Vec<IdentifierType>,
+}
+
+impl IdentifierBuilder {
+    pub fn new() -> Self {
+        IdentifierBuilder {
+            name: None,
+            data: Vec::new(),
+        }
+    }
+
+    pub fn name(&mut self, name: &str) -> &mut Self {
+        self.name = Some(name.to_string());
+        self
+    }
+
+    pub fn add(&mut self, type_: IdentifierTypeName) -> &mut Self {
+        self.data.push(IdentifierType::new(type_));
+        self
+    }
+
+    pub fn build(&self) -> Identifier {
+        Identifier {
+            name: self.name.clone(),
+            data: self.data.clone(),
+        }
     }
 }
