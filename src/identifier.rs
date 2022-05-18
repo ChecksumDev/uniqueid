0969ef458997be30039c871b52e89f1b456bbc35
Checksum <checksum@velvet.moe>
2022-05-18 16:43:18 -0500

    fix: :art: Rename some structs, reformat the codebase, documentation and tests!
    Breaks any code using `IdentifierTypeName`, Update usages to `IdentiferType`

--- src/identifier.rs
+++ src/identifier.rs
@@ -1,18 +1,11 @@
+use std::fmt::Display;
+
 use sha3::{Digest, Sha3_512};
 use sysinfo::{self, DiskExt, ProcessorExt, System, SystemExt};
 
-/// IdentifierTypeName are the available types of identifiers.
-/// # Examples
-/// ```
-/// use uniqueid::identifier::IdentifierTypeName;
-/// assert_eq!(IdentifierTypeName::CPU.as_str(), "CPU");
-/// ```
-/// # See Also
-/// * [IdentifierType](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierType.html)
-/// * [Identifier](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.Identifier.html)
-/// * [IdentifierTypeData](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierTypeData.html)
+/// Enum representing the different types of possible identifiers
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum IdentifierTypeName {
+pub enum IdentifierType {
     CPU,
     // GPU, // TODO: Add GPU support
     RAM,
@@ -21,44 +14,36 @@ pub enum IdentifierTypeName {
     // OS, // TODO: Add OS identifier
 }
 
-impl IdentifierTypeName {
+impl IdentifierType {
+    /// Returns the identifier type as a string
     pub fn as_str(&self) -> &'static str {
         match self {
-            IdentifierTypeName::CPU => "CPU",
-            // IdentifierTypeName::GPU => "GPU",
-            IdentifierTypeName::RAM => "RAM",
-            IdentifierTypeName::DISK => "DISK",
-            // IdentifierTypeName::NET => "NET",
-            // IdentifierTypeName::OS => "OS",
+            IdentifierType::CPU => "CPU",
+            // IdentifierType::GPU => "GPU",
+            IdentifierType::RAM => "RAM",
+            IdentifierType::DISK => "DISK",
+            // IdentifierType::NET => "NET",
+            // IdentifierType::OS => "OS",
         }
     }
 }
 
-impl From<&str> for IdentifierTypeName {
+impl From<&str> for IdentifierType {
+    /// Converts a string to an IdentifierType
     fn from(name: &str) -> Self {
         match name {
-            "CPU" => IdentifierTypeName::CPU,
-            // "GPU" => IdentifierTypeName::GPU,
-            "RAM" => IdentifierTypeName::RAM,
-            "DISK" => IdentifierTypeName::DISK,
-            // "NET" => IdentifierTypeName::NET,
-            // "OS" => IdentifierTypeName::OS,
+            "CPU" => IdentifierType::CPU,
+            // "GPU" => IdentifierType::GPU,
+            "RAM" => IdentifierType::RAM,
+            "DISK" => IdentifierType::DISK,
+            // "NET" => IdentifierType::NET,
+            // "OS" => IdentifierType::OS,
             _ => panic!("Unknown identifier type name: {}", name),
         }
     }
 }
 
-/// The IdentifierTypeData object is the data associated with an IdentifierType.
-/// # Examples
-/// ```
-/// use uniqueid::identifier::IdentifierTypeData;
-///
-/// let data = IdentifierTypeData::new("key", "value");
-/// assert_eq!(data.key, "key");
-/// assert_eq!(data.value, "value");
-/// ```
-/// # See Also
-/// * [IdentifierType](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierType.html)
+/// A struct representing the key-value pairs of an identifier's type data.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct IdentifierTypeData {
     /// The key of the IdentifierTypeData object.
@@ -67,49 +52,22 @@ pub struct IdentifierTypeData {
     pub value: String,
 }
 
-pub struct IdentifierTypeDataBuilder {
-    r#type: IdentifierTypeName,
-    data: Vec<IdentifierTypeData>,
-}
-
-impl IdentifierTypeDataBuilder {
-    pub fn new(r#type: IdentifierTypeName) -> Self {
-        IdentifierTypeDataBuilder {
-            r#type,
-            data: Vec::new(),
-        }
-    }
-
-    pub fn add(&mut self, key: &str, value: &str) -> &mut Self {
-        self.data.push(IdentifierTypeData {
-            key: key.to_string(),
-            value: value.to_string(),
-        });
-        self
-    }
-
-    pub fn build(self) -> String {
-        // Build the IdentifierTypeData objects in format:
-        // TYPE_NAME(key=value, key=value, key=value ...)
-        let mut data = String::new();
-        data.push_str(self.r#type.as_str());
-
-        data.push('(');
-
-        for item in self.data {
-            data.push_str(&format!("{}={}, ", item.key, item.value));
-        }
-        data.pop();
-        data.pop();
-
-        data.push(')');
-
-        data
-    }
-}
-
 impl IdentifierTypeData {
-    /// Creates a new IdentifierTypeData object.
+    /// Creates a new IdentifierTypeData object
+    /// # Arguments
+    /// * `key` - The key of the IdentifierTypeData object
+    /// * `value` - The value of the IdentifierTypeData object
+    /// # Example
+    /// ```
+    /// use uniqueid::identifier::IdentifierTypeData;
+    ///
+    /// let data = IdentifierTypeData::new("key", "value");
+    ///
+    /// assert_eq!(data.key, "key");
+    /// assert_eq!(data.value, "value");
+    /// ```
+    /// # Returns
+    /// * IdentifierTypeData - The new IdentifierTypeData object
     pub fn new(key: &str, value: &str) -> Self {
         IdentifierTypeData {
             key: key.to_string(),
@@ -121,8 +79,10 @@ impl IdentifierTypeData {
     /// # Examples
     /// ```
     /// use uniqueid::identifier::IdentifierTypeData;
+    ///
     /// let data = IdentifierTypeData::new("key", "value");
-    /// assert_eq!(data.key(), "key");
+    ///
+    /// assert_eq!(data.key, "key");
     /// ```
     pub fn key(&self) -> &str {
         &self.key
@@ -132,50 +92,143 @@ impl IdentifierTypeData {
     /// # Examples
     /// ```
     /// use uniqueid::identifier::IdentifierTypeData;
+    ///
     /// let data = IdentifierTypeData::new("key", "value");
-    /// assert_eq!(data.value(), "value");
+    ///
+    /// assert_eq!(data.value, "value");
     /// ```
     pub fn value(&self) -> &str {
         &self.value
     }
+}
 
+impl Display for IdentifierTypeData {
     /// Returns the key and value in normal format. (key=value)
     /// # Examples
     /// ```
     /// use uniqueid::identifier::IdentifierTypeData;
+    ///
     /// let data = IdentifierTypeData::new("key", "value");
+    ///
     /// assert_eq!(data.to_string(), "key=value");
     /// ```
-    pub fn to_string(&self) -> String {
-        format!("{}={}", self.key, self.value)
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}={}", self.key, self.value)
+    }
+}
+
+/// A helper struct for building IdentifierTypeData objects.
+pub struct IdentifierTypeDataBuilder {
+    identifier: IdentifierType,
+    data: Vec<IdentifierTypeData>,
+}
+
+impl IdentifierTypeDataBuilder {
+    /// Creates a new IdentifierTypeDataBuilder object.
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierTypeDataBuilder;
+    /// use uniqueid::identifier::IdentifierType;
+    ///
+    /// let builder = IdentifierTypeDataBuilder::new(IdentifierType::CPU);
+    /// ```
+    /// # Panics
+    /// Panics if the identifier type is not valid.
+    /// ```
+    pub fn new(identifier: IdentifierType) -> Self {
+        IdentifierTypeDataBuilder {
+            identifier,
+            data: Vec::new(),
+        }
+    }
+
+    /// Adds a key-value pair to the IdentifierTypeDataBuilder object.
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierTypeDataBuilder;
+    /// use uniqueid::identifier::IdentifierType;
+    ///
+    /// let mut builder = IdentifierTypeDataBuilder::new(IdentifierType::CPU);
+    /// builder.add("key", "value");
+    /// ```
+    /// # Panics
+    /// Panics if the IdentifierTypeDataBuilder object is empty.
+    /// ```
+    pub fn add<T: Into<String>>(&mut self, key: T, value: T) -> &mut Self {
+        self.data.push(IdentifierTypeData {
+            key: key.into(),
+            value: value.into(),
+        });
+
+        self
+    }
+
+    /// Builds the IdentifierTypeData object into a string.
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierTypeDataBuilder;
+    /// use uniqueid::identifier::IdentifierType;
+    ///
+    /// let mut builder = IdentifierTypeDataBuilder::new(IdentifierType::CPU);
+    ///
+    /// builder.add("key", "value");
+    ///
+    /// assert_eq!(builder.build(), "CPU(key=value)");
+    /// ```
+    pub fn build(self) -> String {
+        let mut data = String::new();
+
+        data.push_str(self.identifier.as_str());
+        data.push('(');
+
+        for item in self.data {
+            data.push_str(&format!("{}={}, ", item.key, item.value));
+        }
+
+        data.pop();
+        data.pop();
+
+        data.push(')');
+
+        data
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct IdentifierType {
-    /// The name of the IdentifierType object.
-    pub name: IdentifierTypeName,
-    /// The data of the IdentifierType object.
+pub struct IdentifierTypeDataList {
+    /// The name of the IdentifierType object. (CPU, RAM, DISK, ...)
+    pub identifier: IdentifierType,
+    /// The data of the IdentifierType object. (key=value, key=value, key=value ...)
     pub data: Vec<IdentifierTypeData>,
 }
 
-impl IdentifierType {
+impl IdentifierTypeDataList {
     /// Creates a new IdentifierType object.
-    pub fn new(r#type: IdentifierTypeName) -> Self {
-        IdentifierType {
-            name: r#type,
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierTypeDataList;
+    /// use uniqueid::identifier::IdentifierType;
+    ///
+    /// let data = IdentifierTypeDataList::new(IdentifierType::CPU);
+    /// ```
+    /// # Panics
+    /// Panics if the identifier type is not valid.
+    /// ```
+    pub fn new(identifier: IdentifierType) -> Self {
+        IdentifierTypeDataList {
+            identifier,
             data: Vec::new(),
         }
     }
 
     pub fn build(&self) -> String {
-        match self.name {
-            IdentifierTypeName::CPU => self.build_cpu(),
-            // IdentifierTypeName::GPU => self.build_gpu(),
-            IdentifierTypeName::RAM => self.build_ram(),
-            IdentifierTypeName::DISK => self.build_disk(),
-            // IdentifierTypeName::NET => self.build_net(),
-            // IdentifierTypeName::OS => self.build_os(),
+        match self.identifier {
+            IdentifierType::CPU => self.build_cpu(),
+            // IdentifierType::GPU => self.build_gpu(),
+            IdentifierType::RAM => self.build_ram(),
+            IdentifierType::DISK => self.build_disk(),
+            // IdentifierType::NET => self.build_net(),
+            // IdentifierType::OS => self.build_os(),
         }
     }
 
@@ -192,7 +245,7 @@ impl IdentifierType {
 
         let mut result = String::new();
 
-        let mut identifier_type = IdentifierTypeDataBuilder::new(IdentifierTypeName::CPU);
+        let mut identifier_type = IdentifierTypeDataBuilder::new(IdentifierType::CPU);
         identifier_type.add("b", brand.to_lowercase().trim());
         identifier_type.add("v", vendor.to_lowercase().trim());
         identifier_type.add("f", &frequency.to_string());
@@ -211,7 +264,7 @@ impl IdentifierType {
 
         let mut result = String::new();
 
-        let mut identifier_type = IdentifierTypeDataBuilder::new(IdentifierTypeName::RAM);
+        let mut identifier_type = IdentifierTypeDataBuilder::new(IdentifierType::RAM);
         identifier_type.add("t", &ram.to_string());
         result.push_str(&identifier_type.build());
 
@@ -234,7 +287,7 @@ impl IdentifierType {
 
             let total_space = disk.total_space();
 
-            let mut identifier_type = IdentifierTypeDataBuilder::new(IdentifierTypeName::DISK);
+            let mut identifier_type = IdentifierTypeDataBuilder::new(IdentifierType::DISK);
             identifier_type.add("t", &total_space.to_string());
             result.push_str(&identifier_type.build());
         }
@@ -248,14 +301,14 @@ pub struct Identifier {
     /// The name of the Identifier.
     pub name: Option<String>,
     /// The data of the Identifier.
-    pub data: Vec<IdentifierType>,
+    pub data: Vec<IdentifierTypeDataList>,
 }
 
 impl Identifier {
-    pub fn new(name: Option<&str>, data: Vec<IdentifierType>) -> Identifier {
+    pub fn new<T: Into<String>>(name: T) -> Self {
         Identifier {
-            name: name.map(|s| s.to_string()),
-            data,
+            name: Some(name.into()),
+            data: Vec::new(),
         }
     }
 
@@ -264,6 +317,7 @@ impl Identifier {
     /// * `hash` - If true, the Identifier will be hashed with SHA3-512.
     pub fn build(&self, hash: bool) -> String {
         let mut result = String::new();
+
         if let Some(name) = &self.name {
             result.push_str(name);
         }
@@ -290,51 +344,109 @@ impl Identifier {
     }
 }
 
-/// Builds an Identifier object.
-/// # Examples
-/// ```
-/// use uniqueid::identifier::{IdentifierBuilder, IdentifierTypeName};
-///
-/// let identifier = IdentifierBuilder::new()
-///    .name("name") // Optional
-///     .add(IdentifierTypeName::CPU)
-///     .add(IdentifierTypeName::RAM)
-///     .add(IdentifierTypeName::DISK)
-///     .build();
-/// ```
-/// # See Also
-/// * [Identifier](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.Identifier.html)
-/// * [IdentifierType](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierType.html)
-/// * [IdentifierTypeName](https://docs.rs/uniqueid/latest/uniqueid/identifier/enum.IdentifierTypeName.html)
-/// * [IdentifierTypeData](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierTypeData.html)
-/// * [IdentifierTypeDataBuilder](https://docs.rs/uniqueid/latest/uniqueid/identifier/struct.IdentifierTypeDataBuilder.html)
+/// IdentifierBuilder is a helper struct for building Identifier objects.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct IdentifierBuilder {
-    name: Option<String>,
-    data: Vec<IdentifierType>,
+    pub name: Option<String>,
+    pub data: Vec<IdentifierTypeDataList>,
 }
 
-impl IdentifierBuilder {
-    pub fn new() -> Self {
-        IdentifierBuilder {
+impl Default for IdentifierBuilder {
+    fn default() -> Self {
+        Self {
             name: None,
             data: Vec::new(),
         }
     }
+}
 
-    pub fn name(&mut self, name: &str) -> &mut Self {
-        self.name = Some(name.to_string());
-        self
-    }
-
-    pub fn add(&mut self, type_: IdentifierTypeName) -> &mut Self {
-        self.data.push(IdentifierType::new(type_));
-        self
-    }
-
-    pub fn build(&self) -> Identifier {
-        Identifier {
-            name: self.name.clone(),
-            data: self.data.clone(),
+impl IdentifierBuilder {
+    /// Creates a new IdentifierBuilder object.
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierBuilder;
+    /// let builder = IdentifierBuilder::default();
+    /// ```
+    pub fn new<T: Into<String>>(name: Option<T>, data: Vec<IdentifierTypeDataList>) -> Self {
+        if let Some(name) = name {
+            IdentifierBuilder {
+                name: Some(name.into()),
+                data,
+            }
+        } else {
+            IdentifierBuilder { name: None, data }
         }
+    }
+
+    /// Sets the name of the Identifier.
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierBuilder;
+    /// let mut builder = IdentifierBuilder::default();
+    /// builder.name("test");
+    ///
+    /// assert_eq!(builder.name, Some("test".to_string()));
+    /// ```
+    /// # Panics
+    /// Panics if the name is not valid.
+    pub fn name<T: Into<String>>(&mut self, name: T) -> &mut Self {
+        self.name = Some(name.into());
+        self
+    }
+
+    /// Adds a new IdentifierType object to the IdentifierBuilder.
+    /// # Arguments
+    /// * `identifier` - The IdentifierType object to add.
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierBuilder;
+    /// use uniqueid::identifier::IdentifierType;
+    ///
+    /// let mut builder = IdentifierBuilder::default();
+    /// builder.add(IdentifierType::CPU);
+    ///
+    /// assert_eq!(builder.data.len(), 1);
+    /// ```
+    /// # Panics
+    /// Panics if the IdentifierType is not valid.
+    pub fn add(&mut self, identifier: IdentifierType) -> &mut Self {
+        self.data.push(IdentifierTypeDataList::new(identifier));
+        self
+    }
+
+    /// Returns an Identifier object from the IdentifierBuilder.
+    /// # Examples
+    /// ```
+    /// use uniqueid::identifier::IdentifierBuilder;
+    /// use uniqueid::identifier::IdentifierType;
+    ///
+    /// let mut builder = IdentifierBuilder::default();
+    /// builder.add(IdentifierType::CPU);
+    ///
+    /// let identifier = builder.build();
+    /// ```
+    pub fn build(self) -> Identifier {
+        Identifier {
+            name: self.name,
+            data: self.data,
+        }
+    }
+}
+
+mod tests {
+    #![allow(unused_imports)]
+    use super::*;
+
+    #[test]
+    fn test_identifier_builder() {
+        let mut builder = IdentifierBuilder::default();
+
+        builder.name("test");
+        builder.add(IdentifierType::CPU);
+
+        let identifier = builder.build();
+
+        assert_eq!(identifier.name, Some("test".to_string()));
+        assert_eq!(identifier.data.len(), 1);
     }
 }
